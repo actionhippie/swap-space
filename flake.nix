63635e1b06d5d9fcf36fa93c5fcf60c9110a5379
Thomas Boerger <thomas@webhippie.de>
2025-09-28 21:00:43 +0200

    chore: add pre-commit-config and update yamllint

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "Nix flake for development";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          imports = [
+            {
+              _module.args.pkgs = import inputs.nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+              };
+            }
+          ];
+
+          devenv = {
+            shells = {
+              default = {
+                git-hooks = {
+                  hooks = {
+                    nixfmt-rfc-style = {
+                      enable = true;
+                    };
+                  };
+                };
+
+                packages = with pkgs; [
+                  nixfmt-rfc-style
+                  yamllint
+                ];
+              };
+            };
+          };
+        };
+    };
+}
